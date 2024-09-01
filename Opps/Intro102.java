00f11ec8b3d554d1ea62c5b7540ff77ee94e7ea4
Harshal <thakareharshal73@gmailcom>
2024-09-01 12:42:04 +0530

    Opps

--- Opps/Intro102.java
+++ Opps/Intro102.java
@@ -1,4 +1,4 @@
-public class Intro02 {
+public class Intro102 {
     public static void main(String[] args) {
 
         final A kunal = new A("Harshal");
---
+++ Opps/smallPack201/Accessingpack1.java
@@ -0,0 +1,7 @@
+package smallPack201;
+
+public class Accessingpack1 {
+    public void Greeting() {
+        System.out.println("hello");
+    }
+}
