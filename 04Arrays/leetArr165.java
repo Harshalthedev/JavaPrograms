4609fde5ccc30461e0e13b85f0bbf395a6808743
Harshal <thakareharshal73@gmailcom>
2025-09-08 17:00:49 +0530

    LeetArr

--- 04Arrays/leetArr165.java
+++ 04Arrays/leetArr165.java
@@ -2,36 +2,28 @@ public class leetArr165 {
     public static void main(String[] args) {
         System.out.println(islandPerimeter(new int[][] {{0,1,0,0},{1,1,1,0},{0,1,0,0},{1,1,0,0}}));
     }
-    private static int peri = 0;
-    private static int m;
-    private static int n;
+  
     public static int islandPerimeter(int[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
         for (int i = 0; i < m; i++) {
-            boolean found = false;
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    dfs(grid, i, j);
-                    found = true;
-                    break;
+                    return dfs(grid, i, j, m, n);
                 }
             }
-            if(found) break;
         }
-        return peri;
+        return -1;
     }
-    private static void dfs(int[][] grid, int i, int j) {
-        if (i >= m || i < 0 || j < 0 || j >= n || grid[i][j] == 0) {
-            peri++;
-            return;
-        } 
-        if (grid[i][j] == -1) return;
+    private static int dfs(int[][] grid, int i, int j, int m, int n) {
+        if (i >= m || i < 0 || j < 0 || j >= n || grid[i][j] == 0) return 1;
+         
+        if (grid[i][j] == -1) return 0;
         grid[i][j] = -1;
 
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
+        return dfs(grid, i+1, j, m, n)
+        + dfs(grid, i-1, j, m, n)
+        + dfs(grid, i, j+1, m, n)
+        + dfs(grid, i, j-1, m, n);
     }
 }
